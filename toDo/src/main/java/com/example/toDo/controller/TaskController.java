0303c53e2b9f68ae9e9f23e6bc0e1c1984f01beb
Raj <ankitkumarraj1999@gmail.com>
2025-06-22 23:06:38 +0530

    Implementing JWT features but not completed yet

--- toDo/src/main/java/com/example/toDo/controller/TaskController.java
+++ toDo/src/main/java/com/example/toDo/controller/TaskController.java
@@ -1,9 +1,11 @@
 package com.example.toDo.controller;
 
+import com.example.toDo.dto.TaskDTO;
 import com.example.toDo.model.Task;
 import com.example.toDo.repository.TaskRepository;
 import com.example.toDo.service.TaskService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,34 +20,36 @@ public class TaskController {
 
     //Get All Tasks
     @GetMapping
-    public List<Task> getAllTasks(){
-        return taskService.getAllTasks();
+    public List<TaskDTO> getAllTasks(){
+        return taskService.getAllTasks().stream()
+                .map(taskService::convertToDTO)
+                .toList();
     }
     //GET task by ID
     @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById(@PathVariable Long id){
-        return taskService.getTaskByID(id).map(ResponseEntity::ok)
+    public ResponseEntity<TaskDTO> getTaskById(@PathVariable Long id){
+        Optional<Task> task = taskService.getTaskByID(id);
+        return task.map(t -> ResponseEntity.ok(taskService.convertToDTO(t)))
                 .orElseGet(()-> ResponseEntity.notFound().build());
     }
 
     //POST - Create new task
     @PostMapping
-    public ResponseEntity<Task> createTask(@Valid @RequestBody Task task){
-        Task saved= taskService.createTask(task);
-        return ResponseEntity.ok(saved);
-
+    public ResponseEntity<TaskDTO> createTask(@Valid @RequestBody TaskDTO dto){
+        Task saved= taskService.createTask(taskService.convertToEntity(dto));
+        return ResponseEntity.ok(taskService.convertToDTO(saved));
     }
     //PUT - Update task
-    @PutMapping("/{id}")
-    public ResponseEntity<Task> updateTask(@PathVariable  Long id, @Valid @RequestBody Task updatedTask){
-        return taskService.updateTask(id,updatedTask)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    @PutMapping
+    public ResponseEntity<TaskDTO> updateTask(@RequestParam @NotNull(message = "ID couldn't be null") Long id,@Valid @RequestBody TaskDTO dto){
+        Optional<Task> updatedTask = taskService.updateTask(id,taskService.convertToEntity(dto));
+        return updatedTask.map(t-> ResponseEntity.ok(taskService.convertToDTO(t)))
+                .orElseGet(()-> ResponseEntity.notFound().build());
     }
 
     //DELETE -task
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTask(@PathVariable Long id){
+    @DeleteMapping
+    public ResponseEntity<Void> deleteTask(@RequestParam Long id){
         taskService.deleteTask(id);
         return ResponseEntity.noContent().build();
     }
--- toDo/src/main/java/com/example/toDo/dto/TaskDTO.java
+++ toDo/src/main/java/com/example/toDo/dto/TaskDTO.java
@@ -1,5 +1,53 @@
 package com.example.toDo.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class TaskDTO {
-    
+
+    @NotBlank(message = "Title cannot be blank")
+    @Size(max = 100,message = "Title cannot be more than 100 characters")
+    private String title;
+
+    private Long id;
+
+    @NotBlank(message = "Description cannot be Blank")
+    private String description;
+
+    private boolean completed;
+
+    //Getter setters
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
 }
